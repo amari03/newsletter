4b598857f957c006dd3cab2301772d10cfcef8cd
amari03 <89270417+amari03@users.noreply.github.com>
2025-02-25 10:25:04 -0600

    Update main_test.go

--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -14,7 +14,7 @@ func TestHomeHandler(t *testing.T) {
     handler := http.HandlerFunc(home)
     // pass the fake HTTP request to the handler
     handler.ServeHTTP(rr, req)
-// perform  the assertions
+	// perform  the assertions
     //1. check the status code for 200 OK
     status := rr.Code
     if status != http.StatusOK {
@@ -22,6 +22,50 @@ func TestHomeHandler(t *testing.T) {
     }
     //2. check the response body
     expected := "Hello from newsletter"
+    got := rr.Body.String()
+    if got != expected {
+      t.Errorf("got %v, expected %v", got, expected)
+    }
+}
+
+func TestAboutHandler(t *testing.T) {
+    // set up request and response
+    req := httptest.NewRequest("GET", "/signup", nil)
+    rr := httptest.NewRecorder()
+    // convert the home  function to a http.Handler type
+    handler := http.HandlerFunc(about)
+    // pass the fake HTTP request to the handler
+    handler.ServeHTTP(rr, req)
+	// perform  the assertions
+    //1. check the status code for 200 OK
+    status := rr.Code
+    if status != http.StatusOK {
+      t.Errorf("got %v, expected %v", status, http.StatusOK)
+    }
+    //2. check the response body
+    expected := "this is the about page"
+    got := rr.Body.String()
+    if got != expected {
+      t.Errorf("got %v, expected %v", got, expected)
+    }
+}
+
+func TestSignupHandler(t *testing.T) {
+    // set up request and response
+    req := httptest.NewRequest("GET", "/signup", nil)
+    rr := httptest.NewRecorder()
+    // convert the home  function to a http.Handler type
+    handler := http.HandlerFunc(signup)
+    // pass the fake HTTP request to the handler
+    handler.ServeHTTP(rr, req)
+	// perform  the assertions
+    //1. check the status code for 200 OK
+    status := rr.Code
+    if status != http.StatusOK {
+      t.Errorf("got %v, expected %v", status, http.StatusOK)
+    }
+    //2. check the response body
+    expected := "this is the signup page"
     got := rr.Body.String()
     if got != expected {
       t.Errorf("got %v, expected %v", got, expected)
